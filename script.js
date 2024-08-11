52b7f7b61ee1aaa86b76f703fef3b95f190d8581
S P Aryan Biswal <141744489+sp-aryan-biswal@users.noreply.github.com>
2024-08-11 23:13:11 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,19 @@
 const scroll = new LocomotiveScroll({
-    el: document.querySelector('#main'),
+  el: document.querySelector("#main"),
+  smooth: true,
+  smartphone: {
     smooth: true,
-    lerp: 0.05
+  },
+
+  tablet: {
+    smooth: true,
+  },
 });
 
-
 gsap.from("h1", {
-    y: 20,
-    duration: 1,
-    delay: 0.8,
-    opacity: 0,
-    stagger: 0.2
-})
+  y: 20,
+  duration: 1,
+  delay: 0.8,
+  opacity: 0,
+  stagger: 0.2,
+});
